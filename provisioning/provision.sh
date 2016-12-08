3e01e7ec2484f745389d0e73c37022e370f6a0dc
gunnaro15 <gunnaro15@ru.is>
2016-12-08 11:24:54 +0000

    address.txt for Jenkins

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -3,5 +3,8 @@
 export INSTANCE=$(aws ec2 run-instances --image-id ami-9398d3e0 --security-group-ids sg-2db63a4b --count 1 --instance-type t2.micro --key-name gunnar-key-pair --query 'Instances[0].InstanceId' --user-data file://init.sh | tr -d '"')
 export ADDRESS=$(aws ec2 describe-instances --instance-ids $INSTANCE --query 'Reservations[0].Instances[0].PublicIpAddress' | tr -d '"')
 
-echo "Adress: $ADDRESS" 
+rm -f address.txt
+echo $ADDRESS >> address.txt
+
+echo "Address: $ADDRESS" 
 
