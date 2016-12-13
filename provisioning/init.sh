8f61e1b3715f053d55087667d43d12c70283fc97
gunnaro15 <gunnaro15@ru.is>
2016-12-13 17:53:00 +0000

    Jenkins API fix

--- provisioning/init.sh
+++ provisioning/init.sh
@@ -4,19 +4,5 @@
 sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
-sudo yum install -y git
-
-sudo su
-
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum -y install nodejs
-
-#git clone https://github.com/gunnaro15/reference-tictactoe.git
-#cd reference-tictactoe
-
 #curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
 #chmod +x ./docker-compose
-
-#npm install
-#sudo ./docker-compose up -d
-#npm run apitest
